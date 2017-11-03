b51d8f1fd5cb268c822f964186d7cfbf32d29d9a
Christopher Donati <cdonati89@gmail.com>
2017-11-03 16:57:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function viewCart() {
 // using object.keys
   var viewPrice = [];
   for (let i = 0; i < cart.length; i++) {
-      viewPrice.push((cart[i].(viewItem[i])));
+      viewPrice.push((cart[i].viewItem[i]));
   }
   //object.values not recognized.  Need to find new way using keys.
   //review .hasOwnProperty to figure out how this would have been done pre-Object.keys
